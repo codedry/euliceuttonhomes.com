d0f4046a047ca5206a2ee76b14f63c77621eef0d
Marcel Morgan <marcel.morgan@codedry.com>
2013-10-03 20:43:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Euhomes::Application.config.secret_key_base = '1e7521119fbe967982797bfa14060a7f1bfa23e53ef5b4ced498b31b06e08545b93e3973b87c7cd2f7b6957741790bb297c497b150c4b78bc0f4335c1d731cfc'
