5be2f8d42ed957acdd1cda09015323746631a90b
Marlon Williams <marlon.willi@gmail.com>
2013-10-05 17:32:25 -0500

    Upgrade lightbox plugin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,14 @@
 class PagesController < ApplicationController
 
+  GALLERY_DIR = Rails.root.join("app/assets/images/gallery_photos")
+
   def aboutus; end
   def services; end
   def contactus; end
   def testimonials; end
-  def gallery; end
+
+  def gallery
+    @gallery_photos = Dir.entries(GALLERY_DIR).keep_if { |filename| filename =~ /.*\.jpg/i }
+  end
 
 end
